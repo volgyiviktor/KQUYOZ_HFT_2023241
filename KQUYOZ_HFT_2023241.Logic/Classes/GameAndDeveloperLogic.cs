831749d6ae86b6ec13e9042a9787e6f84fceff73
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-20 23:30:20 +0100

    GameAndDeveloperLogic megírása

--- KQUYOZ_HFT_2023241.Logic/Classes/GameAndDeveloperLogic.cs
+++ KQUYOZ_HFT_2023241.Logic/Classes/GameAndDeveloperLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KQUYOZ_HFT_2023241.Models;
+using KQUYOZ_HFT_2023241.Repository.Intefaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,36 @@ namespace KQUYOZ_HFT_2023241.Logic.Classes
 {
     public class GameAndDeveloperLogic
     {
+        IRepository<GameAndDeveloper> repo;
 
+        public GameAndDeveloperLogic(IRepository<GameAndDeveloper> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(GameAndDeveloper item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public GameAndDeveloper Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<GameAndDeveloper> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(GameAndDeveloper item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
