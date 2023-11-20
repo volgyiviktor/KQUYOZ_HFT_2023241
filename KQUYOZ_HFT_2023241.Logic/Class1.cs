a82a4bcc8bcc69dc4721a4573266dfdea4b543d2
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-20 23:05:46 +0100

    Logic réteg osztályai és intefészeinek létrehozása

--- KQUYOZ_HFT_2023241.Logic/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KQUYOZ_HFT_2023241.Logic
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ KQUYOZ_HFT_2023241.Logic/Interfaces/IDeveloperLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KQUYOZ_HFT_2023241.Logic.Interfaces
+{
+    internal class IDeveloperLogic
+    {
+    }
+}
