3b3fda0e735b3a628775fab17dbaafb6131f8268
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-20 18:35:49 +0100

    ModelRepository harmadik model

--- KQUYOZ_HFT_2023241.Repository/ModelRepositories/GameRepository.cs
+++ KQUYOZ_HFT_2023241.Repository/ModelRepositories/GameRepository.cs
@@ -1,4 +1,5 @@
 ﻿using KQUYOZ_HFT_2023241.Models;
+using KQUYOZ_HFT_2023241.Repository.Database;
 using KQUYOZ_HFT_2023241.Repository.GenericRepository;
 using KQUYOZ_HFT_2023241.Repository.Intefaces;
 using System;
@@ -9,8 +10,25 @@ using System.Threading.Tasks;
 
 namespace KQUYOZ_HFT_2023241.Repository.ModelRepositories
 {
-    /*public class GameRepository : Repository<Game>, IRepository<Game>
+    public class GameRepository : Repository<Game>, IRepository<Game>
     {
+        public GameRepository(GamesDbContext ctx) : base(ctx)
+        {
+        }
 
-    }*/
+        public override Game Read(int id)
+        {
+            return ctx.GameAndDevelopers.Select(t => t.Game).FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Game item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
 }
