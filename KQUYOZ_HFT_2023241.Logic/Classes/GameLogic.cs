f5e0c6a7edcc3fe1970eecb6f080a7d7cfae7509
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-20 23:32:33 +0100

    GameLogic megírása

--- KQUYOZ_HFT_2023241.Logic/Classes/GameLogic.cs
+++ KQUYOZ_HFT_2023241.Logic/Classes/GameLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KQUYOZ_HFT_2023241.Models;
+using KQUYOZ_HFT_2023241.Repository.Intefaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,36 @@ namespace KQUYOZ_HFT_2023241.Logic.Classes
 {
     public class GameLogic
     {
+        IRepository<Game> repo;
 
+        public GameLogic(IRepository<Game> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Game item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Game Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Game> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Game item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
