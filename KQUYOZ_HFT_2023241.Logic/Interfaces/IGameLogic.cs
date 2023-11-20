0e5a59371edeb5d9623121af7c4754de45d9ca09
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-20 23:13:04 +0100

    IGameLogic megírása

--- KQUYOZ_HFT_2023241.Logic/Interfaces/IGameLogic.cs
+++ KQUYOZ_HFT_2023241.Logic/Interfaces/IGameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KQUYOZ_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace KQUYOZ_HFT_2023241.Logic.Interfaces
 {
     public interface IGameLogic
     {
-
+        void Create(Game item);
+        void Delete(int id);
+        Game Read(int id);
+        IQueryable<Game> ReadAll();
+        void Update(Game item);
     }
 }
