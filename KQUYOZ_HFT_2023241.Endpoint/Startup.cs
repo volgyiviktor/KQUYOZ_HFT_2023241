f351fcb0a6e4669c2ac26b9564deba3e32f5110a
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-23 22:30:03 +0100

    Services beállítása

--- KQUYOZ_HFT_2023241.Endpoint/Startup.cs
+++ KQUYOZ_HFT_2023241.Endpoint/Startup.cs
@@ -1,8 +1,15 @@
+using KQUYOZ_HFT_2023241.Logic.Classes;
+using KQUYOZ_HFT_2023241.Logic.Interfaces;
+using KQUYOZ_HFT_2023241.Models;
+using KQUYOZ_HFT_2023241.Repository.Database;
+using KQUYOZ_HFT_2023241.Repository.Interfaces;
+using KQUYOZ_HFT_2023241.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +23,21 @@ namespace KQUYOZ_HFT_2023241.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<GameDbContext>();
+
+            services.AddTransient<IRepository<Developer>, DeveloperRepository>();
+            services.AddTransient<IRepository<Publisher>, PublisherRepository>();
+            services.AddTransient<IRepository<Game>, GameRepository>();
+
+            services.AddTransient<IDeveloperLogic, DeveloperLogic>();
+            services.AddTransient<IPublisherLogic, PublisherLogic>();
+            services.AddTransient<IGameLogic, GameLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "KQUYOZ_HFT_2023241.EndPoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +46,19 @@ namespace KQUYOZ_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KQUYOZ_HFT_2023241.Endpoint v1"));
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                /*endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                });
+                });*/
+                endpoints.MapControllers();
             });
         }
     }
