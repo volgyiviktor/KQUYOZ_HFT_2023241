952cdfb8822acdb586e1bd928188ecf2dbf923a0
volgyi.viktor <volgy.viktor@DESKTOP>
2023-11-23 23:00:47 +0100

    GameController elkészítése

--- KQUYOZ_HFT_2023241.Endpoint/Controllers/GameController.cs
+++ KQUYOZ_HFT_2023241.Endpoint/Controllers/GameController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KQUYOZ_HFT_2023241.Logic.Interfaces;
+using KQUYOZ_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,6 +11,43 @@ namespace KQUYOZ_HFT_2023241.Endpoint.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+        IGameLogic logic;
+        public GameController(IGameLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Game> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Game Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Game value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Game value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+
+        /*
         // GET: api/<GameController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -40,5 +79,6 @@ namespace KQUYOZ_HFT_2023241.Endpoint.Controllers
         public void Delete(int id)
         {
         }
+        */
     }
 }
